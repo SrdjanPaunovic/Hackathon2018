b195edb5df39967de80594369433980a6a600885
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2018-11-18 13:58:19 +0100

    brute force

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -11,75 +11,52 @@ open('output.txt', 'w').close()
 
 cheapPrice = 0
 BATTERY_MAX_POWER = 5.0
-prev_x = numpy.array([1.0,1.0,1.0, -1])
-def worker(msg: DataMessage) -> ResultsMessage:
+previous_grid_status = True
+blackout_counter = 0
 
+def worker(msg: DataMessage) -> ResultsMessage:
+    global blackout_counter
+    global previous_grid_status
+
+    if not previous_grid_status and msg.grid_status:
+        blackout_counter += 1
+        with open('output.txt', 'a') as f:
+            f.write(str(blackout_counter) + '\n')
+
+    previous_grid_status = msg.grid_status
     if msg.grid_status:
-        return smartGridOn(msg)
+        return gridOn(msg)
     else:
         return gridOff(msg)
-
-
-def getParams(msg: DataMessage) :
-
-    k1 = msg.buying_price
-    k2 = msg.selling_price
-    A = msg.current_load
-    B = msg.solar_production
-
-    def f(x):
-        loadCost = k1*A*(0.2*x[0]+0.4*x[1]+0.4*x[2])
-        oneTimePenaltyCost = 25.0 - 20.0*(x[0] - (1 - prev_x[0])) - 4.0*(x[1] - (1 - prev_x[1])) - x[2]
-        downTimePenalyCost = 1.7 - 1*prev_x[0] - 0.4*prev_x[1] - 0.3*prev_x[2]
-        generatedPrice = (5.0*x[3] + B)*k2
-
-        return loadCost + 22.0*(oneTimePenaltyCost + downTimePenalyCost) - generatedPrice
-    x0_bounds = ( 0, 1)
-    x1_bounds = ( 0, 1)
-    x2_bounds = ( 0, 1)
-    x3_bounds = ( -1, 1)
-    initial_value = prev_x
-    b = [x0_bounds, x1_bounds, x2_bounds, x3_bounds]
-    return minimize(f,x0=initial_value,args=(),method='L-BFGS-B', bounds = b)
-
-def smartGridOn(msg: DataMessage) -> ResultsMessage:
-    res = getParams(msg)
-    global prev_x
-    prev_x = res.x
-    pwr_reference = 0.0
-    boolVector = res.x == 1.0
-    
-    if res.x[3] < 0 and msg.bessSOC < 1:
-        pwr_reference = -5.0  # punjenje
-    elif msg.bessSOC < 0.25:
-        pwr_reference = -0.5  # puni svakako ako je ispod 0.25
-    elif res.x[3] > 0 and msg.bessSOC > 0.27 :
-        pwr_reference = 5  # praznjenje
-
-    return ResultsMessage(data_msg=msg,
-                            load_one = bool(boolVector[0]),
-                            load_two = bool(boolVector[1]),
-                            load_three = bool(boolVector[2]),
-                            power_reference = float(pwr_reference),
-                            pv_mode = PVMode.ON)
 
 def gridOn(msg: DataMessage) -> ResultsMessage:
 
     pwr_reference = 0.0
 
+    load_three = True
+
+    if msg.buying_price == 8 and msg.current_load > 6.5:
+        load_three = False
+
     canCharge = msg.buying_price == 3 or (msg.selling_price == 0 and msg.current_load < msg.solar_production)
     if canCharge and msg.bessSOC < 1:
         pwr_reference = -5.0  # punjenje
     elif msg.bessSOC < 0.25:
-        pwr_reference = -0.1  # puni svakako ako je ispod 0.25
+        pwr_reference = -0.5  # puni svakako ako je ispod 0.25
     elif msg.bessSOC > 0.27 and msg.selling_price == 3 and msg.buying_price == 8:
-        pwr_reference = 5  # praznjenje
+        pwr_reference = 5.0  # praznjenje
+
+    # if blackout_counter == 5 and msg.bessSOC > 0.1 and msg.selling_price == 3:
+    #     pwr_reference = 5.0
+    #
+    # if blackout_counter == 5 and pwr_reference < 0:
+    #     pwr_reference = 0.0
 
     return ResultsMessage(data_msg=msg,
                             load_one= True,
                             load_two= True,
-                            load_three= True,
-                            power_reference = float(pwr_reference),
+                            load_three= load_three,
+                            power_reference = pwr_reference,
                             pv_mode=PVMode.ON)
 
 def gridOff(msg: DataMessage) -> ResultsMessage:
@@ -137,8 +114,8 @@ def gridOff(msg: DataMessage) -> ResultsMessage:
     #                           load_two=True,
     #                           load_three=False,
     #                           power_reference=msg.current_load,
-    #                           pv_mode=PVMode.OFF) 
-                            
+    #                           pv_mode=PVMode.OFF)
+
     #     return ResultsMessage(data_msg=msg,
     #                           load_one=True,
     #                           load_two=True,
